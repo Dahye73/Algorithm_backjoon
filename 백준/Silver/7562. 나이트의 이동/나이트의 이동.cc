2bee0a2f41826fcd22a828a86954b41c6d58c21b
Dahye73 <102746730+Dahye73@users.noreply.github.com>
2024-03-16 01:21:59 +0900

    [Silver I] Title: 나이트의 이동, Time: 160 ms, Memory: 2256 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int dx[8] = { -2, -1, -2, 1, -1, 2, 1, 2 };
+int dy[8] = { -1, -2, 1, -2, 2, -1, 2, 1 };
+int l;
+
+
+
+int bfs(pair<int, int> s, pair<int, int> e, int cnt) {
+
+	vector<int> v;
+	queue<vector<int>> q;
+	int visited[301][301] = { 0, };
+
+	v.push_back(s.first);
+	v.push_back(s.second);
+	v.push_back(cnt);
+
+	q.push(v);
+
+	while (!q.empty())
+	{
+		int x = q.front()[0];
+		int y = q.front()[1];
+		int c = q.front()[2];
+
+		q.pop();
+
+		if (x == e.first && y == e.second) {
+			return c;
+		}
+		for (int i = 0; i < 8; i++) {
+			
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if(0 <= nx && nx < l && 0 <= ny && ny < l && visited[nx][ny] == 0){
+				
+				vector<int> new_v;
+				new_v.push_back(nx);
+				new_v.push_back(ny);
+				new_v.push_back(c + 1);
+
+				q.push(new_v);
+				visited[nx][ny] = 1;
+			}
+		}
+	}
+	return -1;
+}
+
+int main() {
+
+	int t;
+	pair<int, int> start;
+	pair<int, int> end;
+
+	cin >> t;
+	while (t--) {
+		
+		cin >> l;
+		cin >> start.first >> start.second;
+		cin >> end.first >> end.second;
+
+		cout << bfs(start, end, 0) << endl;
+	}
+	return 0;
+}
