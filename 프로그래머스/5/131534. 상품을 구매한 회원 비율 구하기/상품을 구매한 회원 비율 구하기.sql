3b56122c284447a836e0df4d265855224231c7ab
Dahye73 <102746730+Dahye73@users.noreply.github.com>
2024-06-14 00:23:13 +0900

    Update 상품을 구매한 회원 비율 구하기.sql

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -8,15 +8,12 @@
 
 -- 과정
 -- 1. 테이블 조인 (조인 키 : USER_ID)
-
-# WITH USERS AS(
-#     SELECT U.USER_ID, O.SALES_DATE
-#     FROM USER_INFO U
-#     JOIN ONLINE_SALE O
-#     ON U.USER_ID = O.USER_ID
-#     WHERE U.JOINED LIKE '2021-%' 
-#     GROUP BY U.USER_ID
-# )
+-- 2. 가입 시기가 2021년인 사용자 필터링
+-- 3. 연도와 월별로 그룹화 
+--   - DATE_FORMAT 함수를 사용해서 SALES_DATE를 연도(YEAR)와 월(MONTH)로 변환 
+-- 4. 각 그룹별로 구매한 사용자 수 계산
+-- 5. 서브 쿼리를 사용해서 전체 2021년 가입 사용자 계산 후 비율 계산
+-- 6. 정렬
 
 SELECT DATE_FORMAT(O.SALES_DATE, '%Y') AS YEAR,
        DATE_FORMAT(O.SALES_DATE, '%m') AS MONTH,
